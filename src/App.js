84c71786fc51d1cc2965d04baefd45410f079483
evanyang0612 <blacksheet@gmail.com>
2022-10-21 20:28:12 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -76,22 +76,24 @@ export default function App() {
   ));
 
   return (
-    <main>
+    <div>
       {tenzies && <Confetti />}
-      <h1 className="title">Tenzies</h1>
-      <p className="instructions">
-        {tenzies
-          ? "Congratulations! Tenzies again?"
-          : "Roll until all dice are the same. Click each die to freeze it at its current value between rolls."}
-      </p>
-      <div className="dice-container">{diceElements}</div>
-      <button className="roll-dice" onClick={tenzies ? resetGame : rollDice}>
-        {tenzies ? "New Game" : "Roll"}
-      </button>
-      <h3 className="roll-count">Count: {count}</h3>
-      <h3 className="roll-count-best">
-        Best record: {bestCount === Infinity ? "0" : bestCount}
-      </h3>
-    </main>
+      <main>
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">
+          {tenzies
+            ? "Congratulations! Tenzies again?"
+            : "Roll until all dice are the same. Click each die to freeze it at its current value between rolls."}
+        </p>
+        <div className="dice-container">{diceElements}</div>
+        <button className="roll-dice" onClick={tenzies ? resetGame : rollDice}>
+          {tenzies ? "New Game" : "Roll"}
+        </button>
+        <h3 className="roll-count">Count: {count}</h3>
+        <h3 className="roll-count-best">
+          Best record: {bestCount === Infinity ? "0" : bestCount}
+        </h3>
+      </main>
+    </div>
   );
 }
