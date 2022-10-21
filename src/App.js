4fd22dd9b7e6694a1803136010c5ed64da1ca3e4
evanyang0612 <blacksheet@gmail.com>
2022-10-21 20:03:16 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Die from "./Die";
+import "./style.css";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
-function App() {
+export default function App() {
+  const [dice, setDice] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+  const [count, setCount] = useState(0);
+  const [bestCount, setBestCount] = useState(Infinity);
+
+  useEffect(() => {
+    let firsValue = dice[0].value;
+    const allDiceHeld = dice.every((die) => die.isHeld);
+    const allSameValue = dice.every((die) => die.value === firsValue);
+    if (allDiceHeld && allSameValue) {
+      setTenzies(true);
+      handleBestCount();
+    }
+    //eslint-disable-next-line
+  }, [dice]);
+
+  function handleBestCount() {
+    if (count < bestCount) {
+      setBestCount(count);
+    }
+  }
+
+  function resetGame() {
+    setDice(allNewDice());
+    setTenzies(false);
+    setCount(0);
+  }
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDie());
+    }
+    return newDice;
+  }
+
+  function rollDice() {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld ? die : generateNewDie();
+      })
+    );
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        {tenzies
+          ? "Congratulations! Tenzies again?"
+          : "Roll until all dice are the same. Click each die to freeze it at its current value between rolls."}
+      </p>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={tenzies ? resetGame : rollDice}>
+        {tenzies ? "New Game" : "Roll"}
+      </button>
+      <h3 className="roll-count">Count: {count}</h3>
+      <h3 className="roll-count-best">
+        Best record: {bestCount === Infinity ? "0" : bestCount}
+      </h3>
+    </main>
   );
 }
-
-export default App;
